416c3eb7ff0bc541246796a6d1a089429d4bf969
Gunavanth Varma Aduluru <gunavanthvarma@gmail.com>
2026-03-06 17:47:24 -0500

    implemented error constants according to spec

---
+++ AMQPlite/AMQPliteServer/errors/Error.go
@@ -0,0 +1,69 @@
+package errors
+
+import (
+	"fmt"
+)
+
+// according to AMQP 0-9-1 spec
+const (
+	// Normal
+	ReplySuccess uint16 = 200
+	//Channel Exceptions
+	ContentTooLarge    uint16 = 311
+	NoRoute            uint16 = 312
+	NoConsumers        uint16 = 313
+	AccessRefused      uint16 = 403
+	NotFound           uint16 = 404
+	ResourceLocked     uint16 = 405
+	PreconditionFailed uint16 = 406
+	//Connection Exceptions
+	ConnectionForced uint16 = 320
+	InvalidPath      uint16 = 402
+	FrameError       uint16 = 501
+	SyntaxError      uint16 = 502
+	CommandInvalid   uint16 = 503
+	ChannelError     uint16 = 504
+	UnexpectedFrame  uint16 = 505
+	ResourceError    uint16 = 506
+	NotAllowed       uint16 = 530
+	NotImplemented   uint16 = 540
+	InternalError    uint16 = 541
+)
+
+type AMQPError struct {
+	Code     uint16
+	ClassID  uint16
+	MethodID uint16
+	Message  string
+	IsHard   bool // true for connection exceptions, false for channel
+}
+
+func (e *AMQPError) Error() string {
+	errType := "channel"
+	if e.IsHard {
+		errType = "connection"
+	}
+	return fmt.Sprintf("AMQP %s error %d: %s (class: %d, method: %d)", errType, e.Code, e.Message, e.ClassID, e.MethodID)
+}
+
+// close the entire connection
+func NewConnectionError(code uint16, classID uint16, methodID uint16, message string) *AMQPError {
+	return &AMQPError{
+		Code:     code,
+		ClassID:  classID,
+		MethodID: methodID,
+		Message:  message,
+		IsHard:   true,
+	}
+}
+
+// just close the channel
+func NewChannelError(code uint16, classID uint16, methodID uint16, message string) *AMQPError {
+	return &AMQPError{
+		Code:     code,
+		ClassID:  classID,
+		MethodID: methodID,
+		Message:  message,
+		IsHard:   false,
+	}
+}
