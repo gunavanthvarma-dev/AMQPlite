18095aa68e91b06236d2bf1624ba7a8e97409913
Gunavanth Varma Aduluru <gunavanthvarma@gmail.com>
2026-04-03 14:04:42 -0400

    Refactor Queue structure to improve concurrency handling and enhance message forwarding logic

--- AMQPlite/AMQPliteServer/components/Queue.go
+++ AMQPlite/AMQPliteServer/components/Queue.go
@@ -4,6 +4,7 @@ import (
 	"AMQPlite/AMQPliteServer/frames"
 	"context"
 	"log"
+	"sync"
 )
 
 type Queue struct {
@@ -14,13 +15,16 @@ type Queue struct {
 	AutoDelete   bool
 	Exclusive    bool
 
-	MessageCount  uint32
-	ConsumerCount uint32
+	MessageCount    uint32
+	ConsumerCount   uint32
+	roundRobinIndex int
 
 	ExchangeBindings map[string][]*Binding
 	Consumers        map[string]*Consumer
+	consumerKeys     []string
 	ctx              context.Context
 	cancel           context.CancelFunc
+	lock             sync.RWMutex
 }
 
 func NewQueue(name string, durable bool, autoDelete bool, exclusive bool, ctx context.Context, cancel context.CancelFunc) *Queue {
@@ -30,33 +34,55 @@ func NewQueue(name string, durable bool, autoDelete bool, exclusive bool, ctx co
 		Durable:          durable,
 		AutoDelete:       autoDelete,
 		Exclusive:        exclusive,
+		roundRobinIndex:  0,
 		ExchangeBindings: make(map[string][]*Binding),
 		Consumers:        make(map[string]*Consumer),
+		consumerKeys:     make([]string, 0),
 		ctx:              ctx,
 		cancel:           cancel,
 	}
 }
 
 func (queue *Queue) AddConsumer(consumer *Consumer) {
+	queue.lock.Lock()
+	defer queue.lock.Unlock()
 	queue.Consumers[consumer.ConsumerTag] = consumer
+	queue.consumerKeys = append(queue.consumerKeys, consumer.ConsumerTag)
 }
 
-func (queue *Queue) GetConsumer(consumerTag string) *Consumer {
+func (queue *Queue) getConsumer(consumerTag string) *Consumer {
 	return queue.Consumers[consumerTag]
 }
 
 func (queue *Queue) RemoveConsumer(consumerTag string) {
+	queue.lock.Lock()
+	defer queue.lock.Unlock()
 	delete(queue.Consumers, consumerTag)
+	for i, key := range queue.consumerKeys {
+		if key == consumerTag {
+			queue.consumerKeys = append(queue.consumerKeys[:i], queue.consumerKeys[i+1:]...)
+			break
+		}
+	}
 }
 
 func (queue *Queue) ForwardMessages() {
 	for {
 		select {
 		case frame := <-queue.QueueInbound:
+			queue.lock.RLock()
 			log.Printf("[DEBUG] Queue: Forwarding msg to %d consumers\n", len(queue.Consumers))
-			for _, consumer := range queue.Consumers {
-				consumer.ConsumerInbound <- frame
+			if len(queue.consumerKeys) == 0 {
+				//do somehting
+				queue.lock.RUnlock()
+				continue
 			}
+			queue.roundRobinIndex = (queue.roundRobinIndex) % len(queue.consumerKeys)
+			consumer := queue.getConsumer(queue.consumerKeys[queue.roundRobinIndex])
+			queue.roundRobinIndex++
+			queue.lock.RUnlock()
+			consumer.ConsumerInbound <- frame
+
 		case <-queue.ctx.Done():
 			log.Println("Queue cancelled")
 			return
